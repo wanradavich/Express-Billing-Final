6359cbe98015abb32752ae8502f17898ddb2eb7a
Wanrada Pat <wrda.ruks@gmail.com>
2023-12-18 12:24:17 -0800

    manager view complete

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -2,11 +2,6 @@ const express = require("express");
 const indexRouter = express.Router();
 const IndexController = require("../controllers/IndexController");
 
-//home page route
-// indexRouter.get("/", async (req, res) => {
-//     res.render("home", {title: "Home"});
-// });
-
 indexRouter.get("/", IndexController.Index);
 
 module.exports = indexRouter;
